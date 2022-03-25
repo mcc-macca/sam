9a3c6f449721b87b2da339eb0381e53d13f6a864
macca <85993132+mcc-macca@users.noreply.github.com>
2022-03-25 20:38:27 +0100

    Update samjs.esm.js

--- dist/samjs.esm.js
+++ dist/samjs.esm.js
@@ -1,20 +1,4 @@
-/**
- * This is SamJs.js v0.1.2
- *
- * A Javascript port of "SAM Software Automatic Mouth".
- *
- * (c) 2017-2020 Christian Schiffler
- *
- * @link(https://github.com/discordier/sam)
- *
- * @author 2017 Christian Schiffler <c.schiffler@cyberspectrum.de>
- */
-/**
- * Test if a bit is set.
- * @param {Number} bits The bits.
- * @param {Number} mask The mask to test.
- * @return {boolean}
- */
+
 function matchesBitmask (bits, mask) {
   return (bits & mask) !== 0;
 }
